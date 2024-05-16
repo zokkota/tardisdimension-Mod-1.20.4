7aa87854372e4118b3ab66245f7e41e5909580d6
zokkota <theo.franos@gmail.com>
2024-05-16 18:05:27 +0200

    init

---
+++ src/main/java/net/zokkota/tardisdimension/TardisDimension.java
@@ -0,0 +1,15 @@
+package net.zokkota.tardisdimension;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TardisDimension implements ModInitializer {
+	public static final String MOD_ID = "tardisdimension";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/zokkota/tardisdimension/TardisDimensionClient.java
@@ -0,0 +1,10 @@
+package net.zokkota.tardisdimension;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TardisDimensionClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
