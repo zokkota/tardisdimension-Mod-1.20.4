a571b1618ee87f57fe649484d0e99ed7d94083e4
zokkota <theo.franos@gmail.com>
2024-05-17 22:54:58 +0200

    add blocks

--- src/main/java/net/zokkota/tardisdimension/TardisDimension.java
+++ src/main/java/net/zokkota/tardisdimension/TardisDimension.java
@@ -2,14 +2,21 @@ package net.zokkota.tardisdimension;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.zokkota.tardisdimension.block.ModBlocks;
+import net.zokkota.tardisdimension.item.ModItemGroups;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TardisDimension implements ModInitializer {
 	public static final String MOD_ID = "tardisdimension";
+
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		ModItemGroups.registerItemGroups();
+
+		ModBlocks.registerBlocks();
 	}
 }
